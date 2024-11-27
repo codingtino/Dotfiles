29f7c89c1b00fca207a77021f96e5103bf74f60f
t.naumann <t.naumann@gge-germany.de>
2024-11-27 15:19:23 +0100

    fix

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -60,9 +60,11 @@
           "homebrew/homebrew-cask" = inputs.homebrew-cask;
           "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
         ];
+
         brews = [ 
           "neofetch"
         ];
+
         casks = [
 
         ];
@@ -102,32 +104,7 @@
     darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
       modules = [
         configuration
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            # Install Homebrew under the default prefix
-            enable = true;
-
-            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-            enableRosetta = true;
-
-            # User owning the Homebrew prefix
-            user = "t.naumann";
-
-            # Optional: Declarative tap management
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-              "homebrew/homebrew-bundle" = homebrew-bundle;
-            };
-
-            # Optional: Enable fully-declarative tap management
-            #
-            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-            mutableTaps = false;
-          };
-        }
-      ];
+       ];
     };
 
     # Expose the package set, including overlays, for convenience.
