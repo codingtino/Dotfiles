4a6c0e39ce1a35d52f9a30756fe76d3aa95edf98
t.naumann <t.naumann@gge-germany.de>
2024-11-27 11:47:44 +0100

    add home.nix

---
+++ nix-darwin/home.nix
@@ -0,0 +1,41 @@
+# home.nix
+# home-manager switch 
+
+{ config, pkgs, ... }:
+
+{
+  home.username = "t.naumann";
+  home.homeDirectory = "/Users/t.naumann";
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+# Makes sense for user specific applications that shouldn't be available system-wide
+  home.packages = [
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # ".config/nix".source = ~/dotfiles/nix;
+    # ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
+    # ".config/aerospace".source = ~/dotfiles/aerospace;
+  };
+
+  home.sessionVariables = {
+  };
+
+  home.sessionPath = [
+    "/run/current-system/sw/bin"
+      "$HOME/.nix-profile/bin"
+  ];
+  programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      # Add any additional configurations here
+      export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+    '';
+  };
+}
