9c13e412d5e6f29683cdd7f6c0ad49a4216acbc0
t.naumann <t.naumann@gge-germany.de>
2024-11-27 13:08:33 +0100

    remove home-manager output

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -7,7 +7,7 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
