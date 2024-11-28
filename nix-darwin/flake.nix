3a173e7c2487bda03085ea434fd3f012cf1838fa
t.naumann <t.naumann@gge-germany.de>
2024-11-28 13:39:21 +0100

    .

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -28,7 +28,8 @@
       config = { allowUnfree = true; };
     };
 
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
+      nixpkgs.config.allowUnfree = true;
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs;
@@ -37,6 +38,7 @@
           fastfetch
           git
           kitty
+          mkalias
           (vscode-with-extensions.override {
             vscodeExtensions = with vscode-extensions; [
               bbenoist.nix
@@ -75,8 +77,40 @@
         casks = [
           "nikitabobko/tap/aerospace"
         ];
+
+        masApps = {
+          Xcode = 497799835;
+        }
+
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = "true;
+        
       };
 
+      fonts.packages = [
+        (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      ];
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+      '';
 
       # allowUnfree is required to install some packages that are not "free" software.
       nixpkgs.config.allowUnfree = true;
--- setup.sh
+++ setup.sh
@@ -16,13 +16,13 @@ set -euo pipefail
 sh <(curl -L https://nixos.org/nix/install) --yes
 
 ## get Dotfiles
-#/nix/var/nix/profiles/default/bin/nix run --extra-experimental-features nix-command --extra-experimental-features flakes nixpkgs#git clone https://github.com/codingtino/Dotfiles.git ~/.config
-/run/current-system/sw/bin/nix run --extra-experimental-features nix-command --extra-experimental-features flakes nixpkgs#git clone https://github.com/codingtino/Dotfiles.git ~/.config
+/nix/var/nix/profiles/default/bin/nix run --extra-experimental-features nix-command --extra-experimental-features flakes nixpkgs#git clone https://github.com/codingtino/Dotfiles.git ~/.config
+#/nix/var/nix/profiles/default/bin/nix-shell -p git --run 'git clone https://github.com/codingtino/Dotfiles.git ~/.config'
+
 
 ## set hostname
 sed -i '' "s/simple/$(scutil --get LocalHostName)/" ~/.config/nix-darwin/flake.nix
 
 ## isntall nix-darwin
-#/nix/var/nix/profiles/default/bin/nix run nix-darwin -- switch --flake ~/.config/nix-darwin
-/run/current-system/sw/bin/nix run nix-darwin -- switch --flake ~/.config/nix-darwin
+/nix/var/nix/profiles/default/bin/nix run nix-darwin -- switch --flake ~/.config/nix-darwin
 
