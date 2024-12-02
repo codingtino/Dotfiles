aff14ae5de306510b82d088befa52b1ddd85f9de
t.naumann <t.naumann@gge-germany.de>
2024-12-02 10:32:42 +0100

    enable xcode-select installation unintended

--- setup.sh
+++ setup.sh
@@ -11,6 +11,15 @@ set -euo pipefail
 
 ## install xcode
 #xcode-select --install
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 |
+    awk -F"*" '{print $2}' |
+    sed -e 's/^ *//')
+softwareupdate -i "$PROD" --verbose
+rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+
 
 ## install Nix
 sh <(curl -L https://nixos.org/nix/install) --yes
